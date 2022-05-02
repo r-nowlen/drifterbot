ef2f13fa501ff6916db9d70b9485e42af3251e83
Robert Nowlen <robnow839@gmail.com>
2022-05-02 11:02:52 -0400

    added event handler

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const { token } = require('./config.json');
 const fs = require('node:fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -10,11 +22,6 @@ for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
-
-client.once('ready', () => {
-    console.log('Ready!');
-
-});
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
@@ -30,5 +37,6 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
     }
 })
+
 
 client.login(token);
